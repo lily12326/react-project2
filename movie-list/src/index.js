f111af5809ef5fc76b4876c4176001821386d7a0
lily12326 <64433688+lily12326@users.noreply.github.com>
2024-07-10 04:18:42 +0800

    star compo udate

--- movie-list/src/index.js
+++ movie-list/src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useState } from "react";
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 import reportWebVitals from './reportWebVitals';
 
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p> The movie was rated  {movieRating} stars</p>
+    </div>
+
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating maxRating={5} message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+
+    < StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
 
